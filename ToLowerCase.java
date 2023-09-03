3c99e39c340eec7b673886a98b14032ad65e244e
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-09-03 21:38:30 +0530

    Insert ToLowerCase.java

---
+++ ToLowerCase.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    String str;
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter String :");
+    str=s.nextLine();
+    System.out.println(str.toLowerCase()+"lower case for given string");
+
+  }
+}
