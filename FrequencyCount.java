904df234bf43c0ce6cd11a7514c2929481daf4b9
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-09-26 23:55:16 +0530

    FrequencyCountOfGivenCharacter

---
+++ FrequencyCount.java
@@ -0,0 +1,21 @@
+class Frequency {
+
+    public static void main(String[] args) {
+        String str ;
+      char ch;
+      Scanner s=new Scanner(System.in);
+      System.out.println("enter String");
+      str=s.nextLine();
+       System.out.println("enter Character");
+       ch=s.nextLine();
+        int frequency = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(ch == str.charAt(i)) {
+                ++frequency;
+            }
+        }
+
+        System.out.println("Frequency of " + ch + " = " + frequency);
+    }
+}
